dc9babf4fe3a62e12fcad1f23fdf7a0adaa9f0cb
chien <minhchiendc@gmail.com>
2020-07-06 20:01:38 +0700

    Bookstore project use: Angular and Spring boot (luv2code)
    - Create Entity: Product, ProductCategory
    - Create dao: ProductRepository, ProductCategoryRepository

---
+++ src/main/java/com/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,10 @@
+package com.ecommerce.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.ecommerce.entities.ProductCategory;
+@RepositoryRestResource(collectionResourceRel = "productCategory",path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+
+}
