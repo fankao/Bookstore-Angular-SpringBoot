3d9b5133f59297b37cab0ad78063884f425968d0
chien <minhchiendc@gmail.com>
2020-07-11 16:54:10 +0700

    Search for products by category id
    Back-end
    Add findByCategoryId method to ProductRepository

--- src/main/java/com/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/ecommerce/dao/ProductCategoryRepository.java
@@ -1,5 +1,6 @@
 package com.ecommerce.dao;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,5 +10,5 @@ import com.ecommerce.entities.ProductCategory;
 @CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
-
+	
 }
