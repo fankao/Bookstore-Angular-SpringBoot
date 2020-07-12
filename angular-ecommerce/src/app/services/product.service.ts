267cd785e1582c3a1bb1a0a5f0bb2d50e4291b8e
chien <minhchiendc@gmail.com>
2020-07-12 20:53:29 +0700

    Search product for category id (Dynamic category menu)
    Front-end
    - Step 4: Enhance menu component to read data from product service
    - Step 5: Update product service to call url (/product-category) in
    spring boot

--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -3,35 +3,44 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
   private baseUrl = 'http://localhost:8080/api/products';
+  private categoryUrl = 'http://localhost:8080/api/product-category';
   constructor(private httpClient: HttpClient) {}
   /**
    * Search product for category id
-   * @param theCategoryId 
+   * @param theCategoryId
    */
   getProductsList(theCategoryId: number): Observable<Product[]> {
     //need build URL base on category id
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
     return this.httpClient
-      .get<GetRespone>(searchUrl)
+      .get<GetResponeProducts>(searchUrl)
       .pipe(map((respone) => respone._embedded.products));
   }
 
   /**
    * Get all product category
    */
-  getProductCategories() {
-    throw new Error('Method not implemented.');
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient
+      .get<GetResponeProductCategories>(this.categoryUrl)
+      .pipe(map((respone) => respone._embedded.productCategory));
   }
 }
-interface GetRespone {
+interface GetResponeProducts {
   _embedded: {
     products: Product[];
   };
 }
 
+interface GetResponeProductCategories {
+  _embedded: {
+    productCategory: ProductCategory[];
+  };
+}
