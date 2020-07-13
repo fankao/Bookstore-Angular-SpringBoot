94e6176c8a5d969838dbac38a7da08af028c529a
chien <minhchiendc@gmail.com>
2020-07-13 15:05:14 +0700

    Search for products by keyword
    Front-end
    - Step 4: update search component to send data to search route

--- angular-ecommerce/src/app/components/search/search.component.ts
+++ angular-ecommerce/src/app/components/search/search.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
+  constructor(private route: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  doSearch(keyword: string) {
+    console.log(`keyword: ${keyword}`);
+    this.route.navigateByUrl(`/search/${keyword}`);
+    
   }
-
 }
