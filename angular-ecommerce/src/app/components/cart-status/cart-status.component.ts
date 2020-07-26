81105a5bcb91d126196564624bb3399000d5c146
chien <minhchiendc@gmail.com>
2020-07-26 20:11:05 +0700

    Shopping cart status
    Front-end
    Step 5: Subscribe to CartService Display totals

--- angular-ecommerce/src/app/components/cart-status/cart-status.component.ts
+++ angular-ecommerce/src/app/components/cart-status/cart-status.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
-  selector: 'app-cart-status',
-  templateUrl: './cart-status.component.html',
-  styleUrls: ['./cart-status.component.css']
+    selector: 'app-cart-status',
+    templateUrl: './cart-status.component.html',
+    styleUrls: ['./cart-status.component.css'],
 })
 export class CartStatusComponent implements OnInit {
+    totalPrice: number = 0.0;
+    totalQuantity: number = 0;
+    constructor(private cartService: CartService) {}
 
-  constructor() { }
+    ngOnInit(): void {
+        this.updateCartStatus();
+    }
+    updateCartStatus() {
+        //subscribe to cart totalPrice
+        this.cartService.totalPrice.subscribe(
+            (data) => (this.totalPrice = data)
+        );
 
-  ngOnInit(): void {
-  }
-
+        //subscribe to cart totalQuantity
+         this.cartService.totalQuantity.subscribe(
+             (data) => (this.totalQuantity = data)
+         );
+    }
 }
